7959a4a40cef9469da86f031cd1f4f03b3de5457
SupunWellappili <supunchandana1814@gmail.com>
2024-06-13 13:16:38 +0530

    Exact Mapping Servlet (Mapping Specification)

--- MappingSpecification/src/ExactMappingServlet.java
+++ MappingSpecification/src/ExactMappingServlet.java
@@ -4,16 +4,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 
-public class TestDeploymentServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/mapping")
+public class ExactMappingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Request Received");
-        PrintWriter writer = resp.getWriter();
-        writer.print("<h1 style =\"color : red\"> <I> Request Received and Response Generated. </I></h1>");
-
-
+        System.out.println("Customer Get Request Was Received");
     }
 }
