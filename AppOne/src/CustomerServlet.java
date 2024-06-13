35d269a0820b8a488ea30dbb63319c160ab87a14
SupunWellappili <supunchandana1814@gmail.com>
2024-06-14 00:25:42 +0530

    Add MySQL Connection

--- AppOne/src/CustomerServlet.java
+++ AppOne/src/CustomerServlet.java
@@ -4,12 +4,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("App");
+       /* System.out.println("working");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Appone!!");*/
+
+
     }
 }
