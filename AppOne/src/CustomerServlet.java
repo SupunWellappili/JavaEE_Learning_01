28e648611793820cfa93381164dffe4d9bd7aeda
SupunWellappili <supunchandana1814@gmail.com>
2024-06-14 01:00:28 +0530

    Added JDBC Details

--- AppOne/src/CustomerServlet.java
+++ AppOne/src/CustomerServlet.java
@@ -5,6 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 @WebServlet(urlPatterns = "/customer")
@@ -16,5 +20,22 @@ public class CustomerServlet extends HttpServlet {
         writer.write("Appone!!");*/
 
 
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Test", "root", "87654321");
+            Statement stmt = connection.createStatement();
+            ResultSet rst = stmt.executeQuery("select * from customer");
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                Double salary = rst.getDouble(4);
+
+                System.out.println(id+" "+name+" "+address+" "+salary);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
