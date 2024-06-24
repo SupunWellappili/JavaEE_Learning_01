090b12e5df2f056c64805a5501bcf2b1d2b0a736
SupunWellappili <supunchandana1814@gmail.com>
2024-06-24 09:54:23 +0530

    Create Connection & Queries

--- DBCP/src/customerServlet.java
+++ DBCP/src/customerServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 @WebServlet(urlPatterns = "/customer")
@@ -22,6 +26,21 @@ public class customerServlet extends HttpServlet {
         bds.setPassword("87654321");
         bds.setMaxTotal(5);
         bds.setInitialSize(5);
+
+        Connection connection = null;
+        try {
+            connection = bds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Customer");
+            ResultSet rst = pstm.executeQuery();
+            while (rst.next()) {
+                String id = rst.getString(1);
+                System.out.println(id);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
