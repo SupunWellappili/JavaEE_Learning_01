b5d56d005ea97d656f4fe63e4086d33afa7a1b4c
SupunWellappili <supunchandana1814@gmail.com>
2024-06-24 13:28:59 +0530

    Create Item Servlet with doGET

---
+++ DBCP/src/ItemServlet.java
@@ -0,0 +1,36 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+@WebServlet(urlPatterns = "/book")
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource bds = (BasicDataSource) servletContext.getAttribute("bds");
+
+        try {
+            Connection connection = bds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Book");
+            ResultSet rst = pstm.executeQuery();
+
+            while (rst.next()) {
+                String bId = rst.getString(1);
+                System.out.println(bId+", ");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
