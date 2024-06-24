00adc9ff0d61186da3f97fb52784a175db4334fb
SupunWellappili <supunchandana1814@gmail.com>
2024-06-24 09:46:32 +0530

    Create the DBCP pool

--- DBCP/src/customerServlet.java
+++ DBCP/src/customerServlet.java
@@ -1,3 +1,5 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +13,16 @@ public class customerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Got get Method Invoke");    }
+
+        //How to create the DBCP pool
+        BasicDataSource bds = new BasicDataSource();
+        bds.setDriverClassName("com.mysql.jdbc.Driver");
+        bds.setUrl("jdbc:mysql://localhost:3306/Test");
+        bds.setUsername("root");
+        bds.setPassword("87654321");
+        bds.setMaxTotal(5);
+        bds.setInitialSize(5);
+    }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
