bf421bd3076e6998bdbeab0fe09be8bc958976c3
SupunWellappili <supunchandana1814@gmail.com>
2024-06-26 19:06:09 +0530

    MY-COMPANY : IJSE

--- Filters/src/filters/MyFilter.java
+++ Filters/src/filters/MyFilter.java
@@ -2,10 +2,15 @@ package filters;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
-@WebFilter(urlPatterns = {"/customer","/item","/order"})
+//@WebFilter(urlPatterns = {"/customer","/item","/order"})
+@WebFilter(urlPatterns = "/*")
 public class MyFilter implements Filter {
     public MyFilter() {
         System.out.println("Object Created From My Filter");
@@ -23,8 +28,16 @@ public class MyFilter implements Filter {
 
         //before the request send
         System.out.println("Stat");
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
+        //without this line the request will not proceed to the servlet
         filterChain.doFilter(servletRequest,servletResponse);//process request to the servlet
+
+        PrintWriter writer = servletResponse.getWriter();
+        writer.print("MY-COMPANY" +" "+ "IJSE");
+
+        resp.addHeader("MY-COMPANY","IJSE");
 
         //after the request response
         System.out.println("End");
