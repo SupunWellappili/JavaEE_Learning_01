c6e545e8e993a822d179cb486c9913cd367ec71f
SupunWellappili <supunchandana1814@gmail.com>
2024-06-26 14:25:24 +0530

    Filter Run Steps

--- Filters/src/filters/MyFilter.java
+++ Filters/src/filters/MyFilter.java
@@ -5,22 +5,31 @@ import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
 
-@WebFilter(urlPatterns = "/customer")
+@WebFilter(urlPatterns = "/item")
 public class MyFilter implements Filter {
-
+    public MyFilter() {
+        System.out.println("Object Created From My Filter");
+    }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("My Filter initialized");
 
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Stat");
+       // System.out.println("Do Filter Method Called");
+
+        filterChain.doFilter(servletRequest,servletResponse);
+        System.out.println("End");
 
     }
 
     @Override
     public void destroy() {
+        System.out.println("Destroy Filter Method Invoked");
 
     }
 }
